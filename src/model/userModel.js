93d8cf920305532ab124fbfaf47c38a99b20168a
Adarsh Singh <singhadarsh9540@gmail.com>
2025-03-30 01:10:39 +0530

    feat: added schema level validation suing validator npm package

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,5 +1,5 @@
 const { default: mongoose } = require("mongoose");
-
+const validator = require("validator");
 const userSchema = new mongoose.Schema({
     firstName:{
         type:String,
@@ -13,19 +13,32 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        validate: {
+          validator: (value) => validator.isEmail(value),
+          message: "Invalid Email",
+        },
+      },
+      imgURL: {
+        type: String,
+        validate: {
+          validator: (value) => validator.isURL(value),
+          message: "Invalid URL",
+        },
+      },
 
     age:{
         type:Number,
+        min:18
     },
     gender:{
+        enum:['MALE',"FEMALE","OTHER"],
         type:String,
     },
-    
 
 },
 {
