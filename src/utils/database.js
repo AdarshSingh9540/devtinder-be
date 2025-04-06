81850f816bf0bd2b9ea499efe79d2b98dc23eb15
Adarsh Singh <singhadarsh9540@gmail.com>
2025-04-06 12:40:20 +0530

    mongo url add

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 const connectDB = async ()=>{
-    await mongoose.connect(process.env.MONGODB_URL)
+    await mongoose.connect('mongodb+srv://adarsh9540984202:passwordpassword@cluster0.yju1yru.mongodb.net/tinder')
 }
 
 module.exports = connectDB;
